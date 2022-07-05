6897f09e485aec4ba539eb3a773f05f13c7eb9a9
ikeohachidi <ikeohachidi@gmail.com>
2022-07-06 00:22:09 +0100

    feat: add uwb-map-location component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,12 @@
 import { defineNuxtConfig } from 'nuxt'
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 	css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css'],
 	build: {
-		transpile: ['vuetify'],
+		transpile: ['vuetify', '@fawmi/vue-google-maps'],
+	},
+	publicRuntimeConfig: {
+		googleApiKey: ''
 	},
 	vite: {
 		define: {
---
+++ plugins/google-maps.ts
@@ -0,0 +1,9 @@
+import VueGoogleMaps from '@fawmi/vue-google-maps';
+
+export default defineNuxtPlugin(nuxtApp => {
+  nuxtApp.vueApp.use(VueGoogleMaps, {
+    load: {
+      key: nuxtApp.$config.googleApiKey 
+    }
+  })
+})
